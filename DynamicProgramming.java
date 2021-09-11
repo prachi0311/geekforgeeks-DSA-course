0a3ba7a18e2c1a915b57e37341cf0a50d0dea19d
Prachi Singh <nero11223344@gmail.com>
2021-09-11 13:18:50 -0700

    dp

--- DynamicProgramming.java
+++ DynamicProgramming.java
@@ -1,21 +1,98 @@
 package codes;
 
 public class DynamicProgramming {
+	int x=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str1 = "xyztt";
-		String str2 = "xyzt";
+		String str1 = "xyy";
+		String str2 = "xy";
 		
 		//Naive solution
 		//int ans = LCS(str1,str2,str1.length(),str2.length());
 		
 		//By DP tabulation
-		int ans = LCSDP(str1,str2,str1.length(),str2.length());
+		//int ans = LCSDP(str1,str2,str1.length(),str2.length());
+		
+		//Longest Palindromic subsequence
+		//int ans = longestPalindromicSubsequence("geeksforgeeks");
+		
+		//TODO
+		//longestRepeatingSubsequence("abc");
+		
+		//TODO optimized DP solution
+		int ans = optimizedLCS(str1,str2,str1.length(),str2.length());
+		
+		//TODO print LCS
+		//printLCS(str1,str2,str1.length(),str2.length());
 		
 		System.out.println(ans);
+		
+		
 
 	}
 	
+	public static void printLCS(String s1,String s2,int l1,int l2) {
+		String LCS="";
+		int[][] arr = new int[2][l2+1];
+		for(int i=0;i<l2;i++) {
+			arr[0][i]=0;
+		}
+		
+		for(int i=0;i<l1;i++) {
+			for(int j=0;j<l2;j++) {
+				if(s1.charAt(i)==s2.charAt(j)) {
+					arr[1][j+1]=arr[0][j]+1;
+					if(arr[1][j+1]!=arr[0][j+1])
+					LCS=LCS+s1.charAt(i);
+				}
+				else {
+					arr[1][j+1]=Math.max(arr[0][j+1], arr[1][j]);
+				}
+			}
+			for(int k=0;k<l2+1;k++) {
+				arr[0][k]=arr[1][k];
+			}
+		}
+		
+		System.out.println(LCS);
+	}
+	
+	public static int optimizedLCS(String s1,String s2,int l1,int l2) {
+		int[][] arr = new int[2][l2+1];
+		for(int i=0;i<l2;i++) {
+			arr[0][i]=0;
+		}
+		
+		for(int i=0;i<l1;i++) {
+			for(int j=0;j<l2;j++) {
+				if(s1.charAt(i)==s2.charAt(j)) {
+					arr[1][j+1]=arr[0][j]+1;
+				}
+				else {
+					arr[1][j+1]=Math.max(arr[0][j+1], arr[1][j]);
+				}
+			}
+			for(int k=0;k<l2+1;k++) {
+				arr[0][k]=arr[1][k];
+			}
+		}
+		
+		return arr[1][l2];
+	}
+	
+	private static void longestRepeatingSubsequence(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static int longestPalindromicSubsequence(String str1) {
+		// TODO Auto-generated method stub
+		StringBuilder str2 = new StringBuilder(str1);
+		str2.reverse().toString();
+		String strtemp = str2.toString(); 
+		return LCSDP(str1,strtemp,str1.length(),str1.length());
+	}
+
 	public static int LCSDP(String s1,String s2,int l1,int l2) {
 		int[][] table = new int[l1+1][l2+1];
 		for(int i=0;i<l1+1;i++) {
